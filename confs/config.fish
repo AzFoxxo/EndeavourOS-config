855b394aabca3a6e102d1d25a59696c0a9bf0c89
AzTheFox/KnotMasterAz <63619429+KnotMasterAz@users.noreply.github.com>
2021-12-21 14:51:01 +0000

    Installation Script

---
+++ confs/config.fish
@@ -0,0 +1,56 @@
+function gaytext
+    echo $argv | lolcat
+end
+
+function fsck-fix
+   fsck -a $argv
+end
+
+neofetch --ascii_distro anarchy | lolcat
+
+alias fishrc "nvim ~/.config/fish/config.fish; source ~/.config/fish/config.fish"
+alias alacrittyrc "nvim ~/.config/alacritty/alacritty.yml"
+
+alias l "exa -ah"
+alias la 'exa -ah'
+alias ll "exa -lah"
+
+alias d disown
+alias q exit
+alias c clear
+alias b bat
+alias cat bat
+
+alias c. 'code-insiders .'
+alias code. 'code-insiders .'
+
+alias v nvim
+alias vim nvim
+alias vi nvim
+
+alias r 'reboot'
+alias p 'poweroff'
+
+alias ht htop
+alias gt gotop
+alias mem 'watch -n 0.1 free -m'
+alias u 'uname -a'
+alias t tldr
+
+alias rmr 'rm -r'
+alias cpr 'cp -r'
+
+alias dol dolphin
+alias dol. 'dolphin .'
+
+alias up 'yay -Syyu; yay -Scc'
+alias upp 'yay -Syyu; yay -Scc; poweroff'
+alias upr 'yay -Syyu; yay -Scc; reboot'
+
+alias krestart 'pkill plasmashell; plasmashell --replace &; disown; exit;'
+
+alias rm~ 'rm *~*'
+
+export EDITOR=nvim
+
+gaytext "Welcome back $USER!"
---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/sh
+
+# Update
+yay -Syyu
+
+# remove lts kernel, if installed
+sudo pacman -R linux-lts
+
+# Nvida driver
+sudo pacman -S nvidia-installer-dkms
+sudo pacman -S linux-headers
+sudo nvidia-installer-dkms
+
+# Graphics card driver
+yay -S xp-pen-tablet
+
+# VirtualBox
+sudo pacman -S virtualbox virtualbox-guest-iso
+sudo modprobe vboxdrv
+
+sudo pacman -S steam discord telegram-desktop
+
+# Utilities
+yes | sudo pacman -S fish exa bat neovim tldr lolcat fd tmux
+yes | sudo pacman -S alacritty
+
+# Multimedia
+yes | sudo pacman -S kdenlive krita gmic gimp vlc blender
+
+# Development
+yay -S visual-studio-code-insiders-bin
+yay -S unityhub-beta
+
+# Games
+sudo pacman -S minetest supertuxkart
+
+# Emoji support
+yes | sudo pacman -S noto-fonts-emoji
+
+# System monitors
+yes | sudo pacman -S plasma-systemmonitor htop
+yay -S gotop-bin
+
+# Change shell
+chsh -s /usr/bin/fish
+
+# Configs
+rm -r ~/.config/fish; mkdir ~/.config/fish; cp confs/config.fish ~/.config/fish
+rm -r ~/.config/alacritty/; mkdir ~/.config/alacritty; cp confs/alacritty.yml ~/.config/alacritty
+
+# restart
+reboot
