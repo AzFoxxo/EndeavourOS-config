1fa29de5de6e571fa6958666287b36a95315fd80
Az Foxxo <KnotMasterAz@users.noreply.github.com>
2022-02-14 17:32:28 +0000

    Experimental script added

--- sudo-insults.dangerous.sh
+++ sudo-insults.dangerous.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 
 # Must be run with sudo
-echo "Defaults        insults" >> /etc/sudoers
+echo "Defaults        insults" >> /etc/sudoers # sudo insults
+echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub; grub-mkconfig -o /boot/grub/grub.cfg # show other operating systems at boot
