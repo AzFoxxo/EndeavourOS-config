c233a71a60b16961c87c4e191ab396e17d0f102a
Az Foxxo <63619429+KnotMasterAz@users.noreply.github.com>
2022-06-18 20:38:48 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,15 +21,18 @@ yay -S alvr
 yes | sudo pacman -S virtualbox virtualbox-guest-iso
 sudo modprobe vboxdrv
 
-yes | sudo pacman -S steam discord telegram-desktop spotify element-desktop lutris
-yes | yay -S whalebird-bin
-yes | yay -S webcord-git
-yes | yay -S moderndeck-bin
+yes | sudo pacman -S steam spotify lutris
+yes | yay -S whalebird-bin # Mastodon
+yes | yay -S moderndeck-bin # Twitter
+yes | sudo pacman -S discord-canary # Discord
+yes | sudo pacman -S element-desktop # Element
+yes | sudo pacman -S telegram-desktop # Telegram
 
 # Utilities
-yes | sudo pacman -S fish exa bat neovim tldr lolcat fd tmux sshfs
-yes | sudo pacman -S alacritty
-yes | yay -S  ttf-ms-win10-auto
+yes | sudo pacman -S fish exa bat neovim tldr lolcat fd tmux sshfs # Useful command line utilities
+yes | sudo pacman -S alacritty # Alacritty terminal
+yes | sudo pacman -S plasma-systemmonitor htop # System monitors
+yes | yay -S gotop # Pretty terminal system monitor
 
 # Multimedia
 yes | sudo pacman -S kdenlive krita gmic gimp vlc blender audacity inkscpae
@@ -41,30 +44,28 @@ yes | sudo pacman -S dotnet-sdk
 yes | yay -S texlive-core #texlive-full
 
 # Office programs
-yes | sudo pacman -S libreoffice-fresh
-yes | yay -S onlyoffice-bin
-yes | yay -S texlive-full texlive-lang
+yes | sudo pacman -S libreoffice-fresh # Actualy good office program - Libreoffice
+yes | yay -S onlyoffice-bin # MS like office program - OnlyOffice
+yes | yay -S texlive-full texlive-langextra # Latex support
 
-# Games
-yes | sudo pacman -S minetest supertuxkart
+# FOSS Games
+yes | sudo pacman -S minetest # Minetest
+yes | sudo pacman -S supertuxkart # Super Tux Kart
 
-# Wine
+# Wine (Windows compatibility)
 yes | sudo pacman -S wine wine-mono wine-gecko bottles
 yes | yay -S bottles
 
-# Emoji support
-yes | sudo pacman -S noto-fonts-emoji
+# Fonts
+yes | yay -S ttf-ms-win11-auto # Windows fonts
+yes | sudo pacman -S noto-fonts-emoji # Emoji support
 
-# System monitors
-yes | sudo pacman -S plasma-systemmonitor htop
-yes | yay -S gotop
-
-# Change shell
+# Change shell to fish
 chsh -s /usr/bin/fish
 
 # Configs
-rm -r ~/.config/fish; mkdir ~/.config/fish; cp confs/config.fish ~/.config/fish
-rm -r ~/.config/alacritty/; mkdir ~/.config/alacritty; cp confs/alacritty.yml ~/.config/alacritty
+rm -r ~/.config/fish; mkdir ~/.config/fish; cp confs/config.fish ~/.config/fish # Copy fish config
+rm -r ~/.config/alacritty/; mkdir ~/.config/alacritty; cp confs/alacritty.yml ~/.config/alacritty # Copy Alacritty config
 
 # restart
 echo "Press enter to reboot the system now!"
